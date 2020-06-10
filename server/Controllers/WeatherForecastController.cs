ad68bf51625eb02450b016079da295c664fdf694
yoni was <yonixw@gmail.com>
2020-06-10 21:29:27 +0300

    server api works standalonde (dotnet new webapi)

---
+++ server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("w")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+            return 5;
+        }
+    }
+}
