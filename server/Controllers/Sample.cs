3bb63d2a87d015009a86066fc28b4b9292bb417e
yoni was <yonixw@gmail.com>
2020-06-10 22:57:27 +0300

    working with visual studio

--- server/Controllers/Sample.cs
+++ server/Controllers/Sample.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
+// https://aspnetcore.readthedocs.io/en/latest/fundamentals/static-files.html
+
 namespace server.Controllers
 {
     [ApiController]
-    [Route("w")]
+    [Route("sample")]
     public class WeatherForecastController : ControllerBase
     {
         private readonly ILogger<WeatherForecastController> _logger;
--- server/Startup.cs
+++ server/Startup.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -41,6 +44,13 @@ namespace server
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                                    Path.Combine(Directory.GetCurrentDirectory(), @"../client/dist/")),
+                RequestPath = new PathString("/front")
+            });
 
             app.UseEndpoints(endpoints =>
             {
